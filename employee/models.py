d46467d8deaaac5169235ed012cf8f8a7cf746e8
Md-Ali-Azad <aliazad969@gmail.com>
2019-04-20 15:08:06 +0600

    full_form

--- employee/models.py
+++ employee/models.py
@@ -3,7 +3,8 @@ class Employee(models.Model):
     eid = models.CharField(max_length=20)  
     ename = models.CharField(max_length=100)  
     eemail = models.EmailField()  
-    econtact = models.CharField(max_length=15)  
+    econtact = models.CharField(max_length=15)
+    created_at= models.DateTimeField(auto_now_add=True)  
     class Meta:  
         db_table = "employee"  
     def __str__(self):
