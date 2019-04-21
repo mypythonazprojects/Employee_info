569ec5f03a9b558f9f6f55c845bbfea735d2d31e
Md-Ali-Azad <aliazad969@gmail.com>
2019-04-21 12:34:37 +0600

    stylecss

--- employee/forms.py
+++ employee/forms.py
@@ -1,6 +1,10 @@
 from django import forms  
 from employee.models import Employee  
-class EmployeeForm(forms.ModelForm):  
+class EmployeeForm(forms.ModelForm):
+    eid=  forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'id'}),required=True,max_length=100)
+    ename=  forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'name'}),required=True,max_length=100)
+    eemail = forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'email'}),required=True,max_length=100)
+    econtact=  forms.CharField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'contact'}),required=True,max_length=100)
     class Meta:  
         model = Employee  
-        fields = "__all__"  
+        fields = "__all__"
